a86631bb2ffa0c9212499de74de9cd4768e664e0
ashish47b <ashish.sahu@47billion.com>
2018-10-11 15:09:50 +0530

    Google Tag Manager code on contact page and form validation

--- js/scripts.js
+++ js/scripts.js
@@ -1066,46 +1066,31 @@ $(window).load(function () {
 
 
 //Contact Form
+$(document).ready(function() {
+    
+    $('input').on('blur', function() {
+        if ($("#contactForm").valid()) {
+            $('#submit').prop('disabled', false);  
+        } else {
+            $('#submit').prop('disabled', 'disabled');
+        }
+    });
+    
+    $("#contactForm").validate({
+        rules: {
+            ProjectSummary: {
+                required: true,
+                minlength: 4
+            },
+            Email: {
+                required: true,
+                email: true
+            }
+        }
+    });
+});
 
 
-// jQuery(document).ready(function () {
-//
-//     $('#contactform').submit(function () {
-//
-//         var action = $(this).attr('action');
-//
-//         $("#message").slideUp(750, function () {
-//             $('#message').hide();
-//
-//             $('#submit')
-//                 .after('<img src="images/ajax-loader.gif" class="loader" />')
-//                 .attr('disabled', 'disabled');
-//
-//             $.post(action, {
-//                     name: $('#name').val(),
-//                     email: $('#email').val(),
-//                     phone: $('#phone').val(),
-//                     comments: $('#comments').val()
-//                 },
-//                 function (data) {
-//                     document.getElementById('message').innerHTML = data;
-//                     $('#message').slideDown('slow');
-//                     $('#contactform img.loader').fadeOut('slow', function () {
-//                         $(this).remove()
-//                     });
-//                     $('#submit').removeAttr('disabled');
-//                     if (data.match('success') != null) $('#contactform').slideUp('slow');
-//
-//                 }
-//             );
-//
-//         });
-//
-//         return false;
-//
-//     });
-//
-// });
 
 
 //team slider
